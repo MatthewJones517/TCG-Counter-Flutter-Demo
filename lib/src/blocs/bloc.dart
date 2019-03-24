68721e6241ca2a08dde64c0503cf609b00a04e1d
Matthew Jones <matthewjones@Matthews-MacBook-Pro.local>
2019-03-23 20:52:50 -0400

    Set up player score streams

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -7,4 +7,39 @@ import 'package:rxdart/rxdart.dart';
 
 class Bloc {
 
+  // Streams
+  final BehaviorSubject<int> _player1Score = BehaviorSubject<int>();
+  final BehaviorSubject<int> _player2Score = BehaviorSubject<int>();
+
+  // Stream Getters
+  BehaviorSubject<int> get player1ScoreStream => _player1Score.stream;
+  BehaviorSubject<int> get player2ScoreStream => _player2Score.stream;
+
+  Bloc() {
+    init();
+  }
+
+  void init() async {
+    // Set player scores to defaults
+    _player1Score.sink.add(20);
+    _player2Score.sink.add(20);
+  }
+
+  void updateScore({int player, bool addToScore}) async {
+    // Choose the stream we're updating. 
+    BehaviorSubject<int> activeStream;
+    if (player == 1) {
+      activeStream = player1ScoreStream;
+    } else {
+      activeStream = player2ScoreStream;
+    }
+
+    // Update score accordingly
+    if (addToScore) {
+      activeStream.sink.add(activeStream.value++);
+    } else {
+      activeStream.sink.add(activeStream.value--);
+    }
+  }
+
 }
