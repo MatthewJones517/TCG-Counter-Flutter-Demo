4aefe97dd83e525c8c1c87e0f99904926f41d96a
Matthew Jones <matthewjones@Matthews-MacBook-Pro.local>
2019-03-23 21:11:17 -0400

    Set up additional streams and stream manipulators

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -6,14 +6,21 @@ import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 
 class Bloc {
-
   // Streams
   final BehaviorSubject<int> _player1Score = BehaviorSubject<int>();
   final BehaviorSubject<int> _player2Score = BehaviorSubject<int>();
+  final BehaviorSubject<bool> _clickAreaP1Plus = BehaviorSubject<bool>();
+  final BehaviorSubject<bool> _clickAreaP1Minus = BehaviorSubject<bool>();
+  final BehaviorSubject<bool> _clickAreaP2Plus = BehaviorSubject<bool>();
+  final BehaviorSubject<bool> _clickAreaP2Minus = BehaviorSubject<bool>();
 
   // Stream Getters
   BehaviorSubject<int> get player1ScoreStream => _player1Score.stream;
   BehaviorSubject<int> get player2ScoreStream => _player2Score.stream;
+  BehaviorSubject<bool> get clickAreaP1PlusStream => _clickAreaP1Plus.stream;
+  BehaviorSubject<bool> get clickAreaP1MinusStream => _clickAreaP1Minus.stream;
+  BehaviorSubject<bool> get clickAreaP2PlusStream => _clickAreaP2Plus.stream;
+  BehaviorSubject<bool> get clickAreaP2MinusStream => _clickAreaP2Minus.stream;
 
   Bloc() {
     init();
@@ -26,13 +33,11 @@ class Bloc {
   }
 
   void updateScore({int player, bool addToScore}) async {
-    // Choose the stream we're updating. 
-    BehaviorSubject<int> activeStream;
-    if (player == 1) {
-      activeStream = player1ScoreStream;
-    } else {
-      activeStream = player2ScoreStream;
-    }
+    // Choose the stream we're updating.
+    BehaviorSubject<int> activeStream = getScoreStream(
+      player: player,
+      addToScore: addToScore,
+    );
 
     // Update score accordingly
     if (addToScore) {
@@ -40,6 +45,60 @@ class Bloc {
     } else {
       activeStream.sink.add(activeStream.value--);
     }
+
+    activeStream.close();
   }
 
+  BehaviorSubject<int> getScoreStream({int player, bool addToScore}) {
+    BehaviorSubject<int> activeStream;
+    if (player == 1) {
+      activeStream = _player1Score;
+    } else {
+      activeStream = _player2Score;
+    }
+
+    return activeStream;
+  }
+
+  void activateClickArea({int player, bool addToScore}) {
+    // Choose stream we're updating.
+    BehaviorSubject<bool> activeStream = getClickAreaStream(
+      player: player,
+      addToScore: addToScore,
+    );
+
+    activeStream.sink.add(true);
+
+    activeStream.close();
+  }
+
+  BehaviorSubject<bool> getClickAreaStream({int player, bool addToScore}) {
+    // Choose the stream we're updating
+    BehaviorSubject<bool> activeStream;
+
+    if (player == 1) {
+      if (addToScore) {
+        activeStream = _clickAreaP1Plus;
+      } else {
+        activeStream = _clickAreaP1Minus;
+      }
+    } else {
+      if (addToScore) {
+        activeStream = _clickAreaP2Plus;
+      } else {
+        activeStream = _clickAreaP2Minus;
+      }
+    }
+
+    return activeStream;
+  }
+
+  void dispose() {
+    _player1Score.close();
+    _player2Score.close();
+    _clickAreaP1Plus.close();
+    _clickAreaP1Minus.close();
+    _clickAreaP2Plus.close();
+    _clickAreaP2Minus.close();
+  }
 }
