0e25329fc27f860edbc8c4923820a2bc5ba1551f
Matthew Jones <matthewjones@Matthews-MacBook-Pro.local>
2019-03-24 10:23:02 -0400

    Added bloc provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'blocs/provider.dart';
 import 'screens/home_screen.dart';
 
 class App extends StatelessWidget {
   build(context) {
-    return MaterialApp(
-      title: 'TCG Counter',
-      home: Home(),
+    return Provider(
+      child: MaterialApp(
+        title: 'TCG Counter',
+        home: Home(),
+      ),
     );
   }
 }
