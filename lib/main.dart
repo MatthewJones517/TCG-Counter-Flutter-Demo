18539c42edc404922f75928d7aa80237cf0ea45f
Matthew Jones <matthewjones@Matthews-MacBook-Pro.local>
2019-03-31 14:46:56 -0400

    Forced portait mode.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'src/app.dart';
 
-void main() {
+void main() async {
+  // Force portrait mode.
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  
   runApp(App());
 }
