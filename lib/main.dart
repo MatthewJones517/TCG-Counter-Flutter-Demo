e3f8453bd531b5117b8a4b8df5f2c837493a5151
Matthew Jones <matthewjones@Matthews-MacBook-Pro.local>
2019-03-31 15:11:54 -0400

    Removed portrait mode lock.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'src/app.dart';
 
 void main() async {
-  // Force portrait mode.
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   
   runApp(App());
 }
