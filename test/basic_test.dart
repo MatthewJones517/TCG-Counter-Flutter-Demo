324e9f1fade075f711b9f7a0dc3876a13d729526
Matthew Jones <mail@matthewrjones.me>
2020-08-19 12:03:04 -0400

    Added simple unit test

---
+++ test/basic_test.dart
@@ -0,0 +1,16 @@
+/// This is a simple Unit test to make sure counter functionality is working
+/// as expected.
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:tcg_counter/src/app.dart';
+import 'package:tcg_counter/src/widgets/menu_drawer.dart';
+
+void main() {
+  testWidgets('Make sure title and menu display', (WidgetTester tester) async {
+    // Launch app
+    await tester.pumpWidget(App());
+
+    expect(find.text('TCG Counter'), findsOneWidget);
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+  });
+}
